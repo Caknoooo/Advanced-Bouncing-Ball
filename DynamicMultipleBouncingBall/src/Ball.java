ad4363ecde8aa705f7a8a79e5bcc87ac29cc5c37
Caknoooo <caknocomel@gmail.com>
2022-11-25 05:45:12 +0700

    feat: check overlapping ball

--- DynamicMultipleBouncingBall/src/Ball.java
+++ DynamicMultipleBouncingBall/src/Ball.java
@@ -4,34 +4,34 @@ import java.util.Random;
 public class Ball {
 	private float x, y;
 	private float speedX, speedY;
-	private float radius;
+	float radius;
 	private Random rand = new Random();
 	private int red = rand.nextInt(255), green = rand.nextInt(255), blue = rand.nextInt(255);
 	private Font font = new Font("Times New Roman", Font.BOLD, 40);
 	private char c;
-	
+
 	// Constructor
 	public Ball(float x, float y, float radius, float speed, float angleInDegree, char c) {
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
-		this.speedX = (float)(speed * Math.cos(Math.toRadians(angleInDegree)));
-		this.speedY = (float)(speed * Math.sin(Math.toRadians(angleInDegree)));
+		this.speedX = (float) (speed * Math.cos(Math.toRadians(angleInDegree)));
+		this.speedY = (float) (speed * Math.sin(Math.toRadians(angleInDegree)));
 		this.c = c;
 	}
-	
+
 	// Set Grafik
 	public void draw(Graphics g) {
 		g.setColor(new Color(red, green, blue));
-		g.fillOval((int)(x - radius), (int)(y - radius), (int)(2 * radius), (int)(2 * radius));
-		
+		g.fillOval((int) (x - radius), (int) (y - radius), (int) (2 * radius), (int) (2 * radius));
+
 		// Drawing char
 		g.setColor(Color.black);
 		g.setFont(font);
-		
+
 		// Char Position
-		int cX = (int)(x) - (g.getFontMetrics().stringWidth(String.valueOf(c)) / 2);
-		int cY = (int)(y) + (g.getFontMetrics().getHeight() / 4);
+		int cX = (int) (x) - (g.getFontMetrics().stringWidth(String.valueOf(c)) / 2);
+		int cY = (int) (y) + (g.getFontMetrics().getHeight() / 4);
 		g.drawString(String.valueOf(c), cX, cY);
 	}
 
@@ -40,53 +40,51 @@ public class Ball {
 		float ballMinY = box.minY + radius;
 		float ballMaxX = box.maxX - radius;
 		float ballMaxY = box.maxY - radius;
-		
+
 		x += speedX;
 		y += speedY;
 
-		if(x < ballMinX) {
+		if (x < ballMinX) {
 			speedX *= -1;
 			x = ballMinX;
-		}
-		else if(x > ballMaxX) {
+		} else if (x > ballMaxX) {
 			speedX *= -1;
 			x = ballMaxX;
 		}
-		
-		if(y < ballMinY) {
+
+		if (y < ballMinY) {
 			speedY *= -1;
 			y = ballMinY;
-		}
-		else if(y > ballMaxY) {
+		} else if (y > ballMaxY) {
 			speedY *= -1;
 			y = ballMaxY;
 		}
 	}
-	
+
 	public void collide(Ball ball) {
 		float tempSpeedX, tempSpeedY;
-		
+
 		int distX = (int) (ball.x - x);
 		int distY = (int) (ball.y - y);
-		
+
 		double distSquared = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
-		
-		if(distSquared <= ball.radius + radius) {
-			if(distSquared < ball.radius + radius) {
+
+		if (distSquared <= ball.radius + radius) {
+			if (distSquared < ball.radius + radius) {
 				double sinT = distY / (distSquared);
 				double cosT = distX / (distSquared);
-				
+
 				x = ball.x - (float) (cosT * (radius + ball.radius));
 				y = ball.y - (float) (sinT * (radius + ball.radius));
 			}
-			
+
 			tempSpeedX = speedX;
 			tempSpeedY = speedY;
 			speedX = ball.speedX;
 			speedY = ball.speedY;
 			ball.speedX = tempSpeedX;
 			ball.speedY = tempSpeedY;
-			
+
 		}
 	}
 }
--- DynamicMultipleBouncingBall/src/BallPanel.java
+++ DynamicMultipleBouncingBall/src/BallPanel.java
@@ -7,19 +7,20 @@ import java.util.regex.Pattern;
 import java.util.List;
 import java.util.ArrayList;
 
-public class BallPanel extends JPanel implements KeyListener{
+public class BallPanel extends JPanel implements KeyListener {
 	private static final int REFRESH_RATE = 30;
 	private Ball ball;
 	public List<Ball> balls = new ArrayList<Ball>();
 	private BallArea box;
 	private int areaWidth, areaHeight;
-	
+	private int tempX, tempY;
+
 	public BallPanel(int width, int height) {
 		this.areaHeight = height;
 		this.areaWidth = width;
-		
+
 		box = new BallArea(0, 0, width, height);
-	
+
 		// Untuk mendapatkan ukuran area latar belakang jika frame di resize
 		this.addComponentListener(new ComponentAdapter() {
 			@Override
@@ -35,40 +36,52 @@ public class BallPanel extends JPanel implements KeyListener{
 		this.setFocusable(true);
 		startThread();
 	}
-	
+
 	public void startThread() {
 		Thread gameThread = new Thread() {
 			public void run() {
-				while(true) {
+				while (true) {
 					update(box);
 					repaint();
 					try {
 						Thread.sleep(1000 / REFRESH_RATE);
-					} catch(InterruptedException ex) {}
+					} catch (InterruptedException ex) {
+					}
 				}
 			}
 		};
-		
+
 		{
 			gameThread.start(); // Jalankan thread
 		}
+	}
+
+	// Avoid overlapping at start or when random ball
+	private static boolean isOverLapping(int x, int y, int ballX, int ballY, int radius) {
+		int distX = ballX - x;
+		int distY = ballY - y;
+		double distSquare = Math.pow(distX, 2) + Math.pow(distY, 2);
+		if (distSquare <= Math.pow(radius + radius, 2))
+			return true;
+		return false;
 	}
 
 	private int angleInDegree() {
 		Random rand = new Random();
 		return rand.nextInt(360);
 	}
-	
-	private void update(BallArea box){
-		for(Ball ball : balls) {
+
+	private void update(BallArea box) {
+		for (Ball ball : balls) {
 			ball.collide(box);
-			for(Ball ball2 : balls) {
-				if(ball == ball2) continue;
+			for (Ball ball2 : balls) {
+				if (ball == ball2)
+					continue;
 				ball.collide(ball2);
 			}
 		}
 	}
-	
+
 	private void createBall(char c) {
 		Random rand = new Random();
 		int radius = 50;
@@ -76,16 +89,24 @@ public class BallPanel extends JPanel implements KeyListener{
 		int x = rand.nextInt(this.areaWidth - radius * 2 - 20) + radius + 10;
 		int y = rand.nextInt(this.areaHeight - radius * 2 - 20) + radius + 10;
 
-		// Danger OverLapping	
+		if (balls.isEmpty()) {
+			while (isOverLapping(tempX, tempY, x, y, radius)) {
+				x = rand.nextInt(this.areaWidth - radius * 2 - 20) + radius + 10;
+				y = rand.nextInt(this.areaHeight - radius * 2 - 20) + radius + 10;
+			}
+		}
+
+		tempX = x;
+		tempY = y;
 		ball = new Ball(x, y, radius, speed, angleInDegree(), c);
 		balls.add(ball);
 	}
-	
-	@Override 
+
+	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		box.draw(g);
-		for(Ball ball : balls){
+		for (Ball ball : balls) {
 			ball.draw(g);
 		}
 	}
@@ -93,9 +114,10 @@ public class BallPanel extends JPanel implements KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		char c = e.getKeyChar();
-		boolean isCorrect = Pattern.matches("[A-Z]", String.valueOf(c)) || Pattern.matches("[0-9]", String.valueOf(c)) || Pattern.matches("[a-z]", String.valueOf(c));
-		
-		if(isCorrect) {
+		boolean isCorrect = Pattern.matches("[A-Z]", String.valueOf(c)) || Pattern.matches("[0-9]", String.valueOf(c))
+				|| Pattern.matches("[a-z]", String.valueOf(c));
+
+		if (isCorrect) {
 			createBall(c);
 		}
 	}
@@ -103,14 +125,13 @@ public class BallPanel extends JPanel implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
 }
